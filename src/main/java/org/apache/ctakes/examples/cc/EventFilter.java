97c56cf6d8554c6390717ceae5ace232f8d3f9b3
Eli Goldner <egoldner@brandeis.edu>
2023-05-09 11:40:06 -0400

    EventFilter is working as we need it to from what I can tell

--- src/main/java/org/apache/ctakes/examples/cc/EventFilter.java
+++ src/main/java/org/apache/ctakes/examples/cc/EventFilter.java
@@ -4,17 +4,17 @@ import org.apache.ctakes.core.pipeline.PipeBitInfo;
 import org.apache.ctakes.core.resource.FileLocator;
 import org.apache.ctakes.typesystem.type.textsem.EventMention;
 import org.apache.log4j.Logger;
-import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
-import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.cas.TOP;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -22,11 +22,11 @@ import java.util.stream.Collectors;
 @PipeBitInfo(
         name = "EventFilter ( TxTimelines )",
         description = " test ",
-        dependencies = {PipeBitInfo.TypeProduct.EVENT },
+        dependencies = { PipeBitInfo.TypeProduct.EVENT },
         products = { PipeBitInfo.TypeProduct.EVENT }
 )
 
-public class EventFilter extends JCasAnnotator_ImplBase {
+public class EventFilter extends org.apache.uima.fit.component.JCasAnnotator_ImplBase {
     final static private Logger LOGGER = Logger.getLogger( "EventFilter" );
 
     public static final String PARAM_FILTER_LIST = "filterList";
@@ -35,10 +35,9 @@ public class EventFilter extends JCasAnnotator_ImplBase {
             name = PARAM_FILTER_LIST,
             description = "The way we store files for processing.  Aligned pair of directories "
     )
-
-
     private String filterList;
 
+    /*
     static public AnalysisEngineDescription createEngineDescription( String filterList )
             throws ResourceInitializationException {
         return AnalysisEngineFactory.createEngineDescription(
@@ -55,8 +54,32 @@ public class EventFilter extends JCasAnnotator_ImplBase {
         );
     }
 
-    private final Set<String> terms = getTerms();
+    static public AnalysisEngineDescription getEngineDescription( String filterList )
+            throws ResourceInitializationException {
+        return AnalysisEngineFactory.createEngineDescription(
+                EventFilter.class,
+                EventFilter.PARAM_FILTER_LIST,
+                filterList
+        );
+    }
+
+    static public AnalysisEngineDescription getAnnotatorDescription()
+            throws ResourceInitializationException {
+        return AnalysisEngineFactory.createEngineDescription(
+                EventFilter.class
+        );
+    }
+
+     */
+
+    private Set<String> terms;
+
     @Override
+    public void initialize( UimaContext context ) throws ResourceInitializationException {
+        super.initialize( context );
+        this.terms = getTerms();
+    }
+
     public void process( JCas jCas ) throws AnalysisEngineProcessException {
         JCasUtil.select( jCas, EventMention.class )
                 .stream()
@@ -67,35 +90,27 @@ public class EventFilter extends JCasAnnotator_ImplBase {
                                         term -> eventMention
                                                 .getCoveredText()
                                                 .toLowerCase()
-                                                .contains(
-                                                        term
-                                                )
+                                                .contains( term )
                                 )
-                ).forEach(
-                        TOP::removeFromIndexes
-                );
+                ).forEach( EventMention::removeFromIndexes );
     }
 
     private Set<String> getTerms() {
         if ( filterList != null && !filterList.isEmpty() ) {
-            LOGGER.info( "Using Dictionary Descriptor: " + filterList );
             try ( InputStream descriptorStream = FileLocator.getAsStream( filterList ) ) {
                 return new BufferedReader(
                         new InputStreamReader(
                                 descriptorStream,
                                 StandardCharsets.UTF_8
                         )
-                )
-                        .lines()
-                        .map(String::toLowerCase)
-                        .collect(
-                                Collectors.toSet()
-                        );
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                ).lines()
+                        .map( String::toLowerCase )
+                        .collect( Collectors.toSet() );
+            } catch ( IOException e ) {
+                throw new RuntimeException( e );
             }
         } else {
-            throw new RuntimeException("Missing Filter List");
+            throw new RuntimeException( "Missing Filter List" );
         }
     }
 }
