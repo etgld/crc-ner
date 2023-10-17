8c29f5fb21f6baccc968ff63fc560f23821e36a3
Eli Goldner <egoldner@brandeis.edu>
2023-10-17 16:11:25 -0400

    All Python annotators called in the same *_pipeline.py file to avoid multiple Python steps in the main piper file

--- instance-generator/src/user/resources/org/apache/ctakes/temporal/ctakes_cnlpt_py/src/ctakes_cnlpt/timelines_pipeline.py
+++ instance-generator/src/user/resources/org/apache/ctakes/temporal/ctakes_cnlpt_py/src/ctakes_cnlpt/timelines_pipeline.py
@@ -5,6 +5,9 @@ from ctakes_pbj.component.pbj_sender import PBJSender
 from ctakes_pbj.pipeline.pbj_pipeline import PBJPipeline
 
 from ctakes_cnlpt.ae.conmod_delegator import ConmodDelegator
+from ctakes_cnlpt.ae.doctimerel_delegator import DocTimeRelDelegator
+from ctakes_cnlpt.ae.temporal_delegator import TemporalDelegator
+
 
 warnings.filterwarnings("ignore")
 
@@ -13,7 +16,10 @@ def main():
 
     # Create a new PBJ Pipeline, add a class that interacts with cNLPT to add Negation to Events.
     pipeline = PBJPipeline()
+    # will have to confirm with Jiarui and Guergana but this order should work
     pipeline.add(ConmodDelegator())
+    pipeline.add(DocTimeRelDelegator())
+    pipeline.add(TemporalDelegator())
     # Add a PBJ Sender to the end of the pipeline to send the processed cas back to cTAKES and initialize the pipeline.
     pipeline.add(PBJSender())
     pipeline.initialize()
