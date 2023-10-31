9d6a9e6b3135ab651066c1793000ee933e42d6c9
Eli Goldner <egoldner@brandeis.edu>
2023-10-31 14:05:21 -0400

    Starting to fold in the model loading interface and logic

--- instance-generator/src/user/resources/org/apache/ctakes/timelines/timelines_py/src/timelines/ae/PROTOTYPE_REST_BACKEND.py
+++ instance-generator/src/user/resources/org/apache/ctakes/timelines/timelines_py/src/timelines/ae/PROTOTYPE_REST_BACKEND.py
@@ -22,16 +22,17 @@ from typing import Dict, List, Tuple, Union
 
 import numpy as np
 from fastapi import FastAPI
-from nltk.tokenize import wordpunct_tokenize as tokenize
 from pydantic import BaseModel
 from seqeval.metrics.sequence_labeling import get_entities
 
-from ..CnlpModelForClassification import CnlpConfig, CnlpModelForClassification
+from cnlpt.CnlpModelForClassification import CnlpConfig, CnlpModelForClassification
 from .cnlp_rest import create_instance_string, get_dataset, initialize_cnlpt_model
 
 app = FastAPI()
 logger = logging.getLogger("CNLPT_LEVEL_TIMELINE_REST_PROTOTYPE")
 logger.setLevel(logging.INFO)
+
+
 
 class SentenceDocument(BaseModel):
     sentence: str
@@ -76,3 +77,37 @@ class TemporalResults(BaseModel):
 
 def create_instance_string(tokens: List[str]):
     return " ".join(tokens)
+
+
+@app.on_event("startup")
+async def startup_event(
+        dtr_path: str,
+        tlink_path: str,
+        mod_path: str,
+):
+    pass
+
+
+def rest():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Run the http server for temporal")
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        help="The port number to run the server on",
+        default=8000,
+    )
+
+    args = parser.parse_args()
+
+    import uvicorn
+
+    uvicorn.run(
+        "cnlpt.api.temporal_rest:app", host="0.0.0.0", port=args.port, reload=True
+    )
+
+
+if __name__ == "__main__":
+    rest()
--- instance-generator/src/user/resources/org/apache/ctakes/timelines/timelines_py/src/timelines/timelines_pipeline.py
+++ instance-generator/src/user/resources/org/apache/ctakes/timelines/timelines_py/src/timelines/timelines_pipeline.py
@@ -4,9 +4,7 @@ from ctakes_pbj.component.pbj_receiver import start_receiver
 from ctakes_pbj.component.pbj_sender import PBJSender
 from ctakes_pbj.pipeline.pbj_pipeline import PBJPipeline
 
-from ctakes_cnlpt.ae.conmod_delegator import ConmodDelegator
-from ctakes_cnlpt.ae.doctimerel_delegator import DocTimeRelDelegator
-from ctakes_cnlpt.ae.temporal_delegator import TemporalDelegator
+from timelines.ae.PROTOTYPE_REST_delegator import TemporalDelegator
 
 
 warnings.filterwarnings("ignore")
@@ -16,9 +14,6 @@ def main():
 
     # Create a new PBJ Pipeline, add a class that interacts with cNLPT to add Negation to Events.
     pipeline = PBJPipeline()
-    # will have to confirm with Jiarui and Guergana but this order should work
-    pipeline.add(ConmodDelegator())
-    pipeline.add(DocTimeRelDelegator())
     pipeline.add(TemporalDelegator())
     # Add a PBJ Sender to the end of the pipeline to send the processed cas back to cTAKES and initialize the pipeline.
     pipeline.add(PBJSender())
