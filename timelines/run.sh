f9a0dac2947340e1e433bbfbe73637e0f75bafac
Eli Goldner <egoldner@brandeis.edu>
2023-12-13 10:33:31 -0500

    Jar mostly working and run script added, running into an issue with JMS Sender

---
+++ timelines/run.sh
@@ -0,0 +1,10 @@
+# java -cp target/txtimelines-lookup-5.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-dependencies.jar \
+     org.apache.ctakes.core.pipeline.PiperFileRunner \
+     -p org/apache/ctakes/timelines/pipeline/Timelines \
+     -a mybroker \
+     -v /usr/local/miniconda3/envs/timelines-docker \
+     -i ../input/ \
+     -o ../output \
+     -l org/apache/ctakes/dictionary/lookup/fast/bsv/Chemotherapy.xml \
+     --key 9302930b-26f3-497d-8b32-3277c257293c
