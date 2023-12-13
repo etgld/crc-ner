775c5ae18d41b4e98850289683b1d421dcb6d7a4
Eli Goldner <egoldner@brandeis.edu>
2023-12-13 12:32:02 -0500

    clear and rebuild script added

---
+++ timelines/clear_run_debug.sh
@@ -0,0 +1,16 @@
+rm -rf resources;
+rm -rf instance-generator/resources;
+rm -rf tweaked-timenorm/resources;
+mvn clean package;
+java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-dependencies.jar \
+     org.apache.ctakes.core.pipeline.PiperFileRunner \
+     -p org/apache/ctakes/timelines/pipeline/Timelines \
+     -a mybroker \
+     -v /usr/local/miniconda3/envs/timelines-docker \
+     -i ../input/ \
+     -o ../output \
+     -d ../dtr/ \
+     -m ../conmod/ \
+     -t ../tlink/ \
+     -l org/apache/ctakes/dictionary/lookup/fast/bsv/Chemotherapy.xml \
+     --key 9302930b-26f3-497d-8b32-3277c257293c
