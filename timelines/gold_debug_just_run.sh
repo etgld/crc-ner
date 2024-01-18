d0f280b759674b5156f56547b799eb5b3df4937d
Eli Goldner <egoldner@brandeis.edu>
2024-01-18 11:53:23 -0500

    Gold debug variants of the main scripts

---
+++ timelines/gold_debug_just_run.sh
@@ -0,0 +1,49 @@
+# rm -rf resources;
+# rm -rf target;
+# rm -rf instance-generator/resources;
+# rm -rf instance-generator/target;
+# rm -rf tweaked-timenorm/resources;
+# rm -rf tweaked-timenorm/target;
+# mvn package;
+# java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-dependencies.jar \
+    #      org.apache.ctakes.core.pipeline.PiperFileRunner \
+    #      -p org/apache/ctakes/timelines/pipeline/Timelines \
+    #      -a  mybroker \
+    #      -v /usr/local/miniconda3/envs/timelines-docker \
+    #      -i ../input/ \
+    #      -o ../output \
+    #      -d ../dtr/ \
+    #      -m ../conmod/ \
+    #      -t ../tlink/ \
+    #      -l org/apache/ctakes/dictionary/lookup/fast/bsv/Chemotherapy.xml \
+    #      -s 8161 \
+    #      -r 8161 \
+    #      --pipPbj yes \
+    #      --key 9302930b-26f3-497d-8b32-3277c257293c
+# java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-dependencies.jar \
+    #      org.apache.ctakes.core.pipeline.PiperFileRunner \
+    #      -p org/apache/ctakes/timelines/pipeline/Timelines \
+    #      -a  mybroker \
+    #      -v /usr/local/miniconda3/envs/timelines-docker \
+    #      -i ../input/ \
+    #      -o ../output \
+    #      -d ../dtr/ \
+    #      -m ../conmod/ \
+    #      -t ../tlink/ \
+    #      -l org/apache/ctakes/dictionary/lookup/fast/bsv/Chemotherapy.xml \
+    #      --pipPbj yes \
+    #      --key 9302930b-26f3-497d-8b32-3277c257293c
+
+java -cp instance-generator/target/instance-generator-5.0.0-SNAPSHOT-jar-with-dependencies.jar \
+     org.apache.ctakes.core.pipeline.PiperFileRunner \
+     -p org/apache/ctakes/timelines/pipeline/Timelines \
+     -a mybroker \
+     -v /usr/local/miniconda3/envs/timelines-docker \
+     -i ../input/ \
+     -o ../output \
+     -d ../dtr/ \
+     -m ../conmod/ \
+     -t ../tlink/ \
+     -l org/apache/ctakes/dictionary/lookup/fast/bsv/Unified_Gold_Dev.xml \
+     --pipPbj yes \
+     --key 9302930b-26f3-497d-8b32-3277c257293c
